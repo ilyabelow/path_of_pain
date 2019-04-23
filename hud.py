bd424c70e19d8d9370427e7743db99ba242239b8
ilyabelow <belof.ilya@gmail.com>
2019-04-23 19:11:04 +0300

    fixed (some) magic numbers in hud

--- hud.py
+++ hud.py
@@ -27,29 +27,34 @@ class HUD(base.AdvancedSprite):
 
 class HealthHUD(HUD):
     def __init__(self, owner):
-        HUD.__init__(self, owner, (30, 30))
+        HUD.__init__(self, owner, (30, 30))  # this const will be here because it's easier to find it here I GUESS
 
     def makeup(self):
         # IMAGE COMPOSING
-        self.image = pygame.Surface((100 * (self.owner.max_health + self.owner.weak_health), 100), pygame.SRCALPHA, 32)
+        self.image = pygame.Surface(
+            (HEART_SPRITE.get_width() * (self.owner.max_health + self.owner.weak_health), HEART_SPRITE.get_height()),
+            pygame.SRCALPHA, 32)
         # normal hearts
         for i in range(self.owner.max_health):
             if i < self.owner.health:
-                self.image.blit(HEART_SPRITE, (i * 100, 0))
+                self.image.blit(HEART_SPRITE, (i * HEART_SPRITE.get_width(), 0))
             else:
-                self.image.blit(HEART_EMPTY_SPRITE, (i * 100, 0))
+                self.image.blit(HEART_EMPTY_SPRITE, (i * HEART_SPRITE.get_width(), 0))
         # weak hearts
         for i in range(self.owner.weak_health):
-            self.image.blit(HEART_WEAK_SPRITE, ((i + self.owner.max_health) * 100, 0))
+            self.image.blit(HEART_WEAK_SPRITE, ((i + self.owner.max_health) * HEART_SPRITE.get_width(), 0))
 
 
 class KeyHUD(HUD):
     def __init__(self, owner):
-        HUD.__init__(self, owner, (30, 150-120*owner.game.painful))
+        HUD.__init__(self, owner, (30, 150 - 120 * owner.game.painful))  # keys HUD will be placed in place of hearts
 
     def makeup(self):
         # IMAGE COMPOSING
-        self.image = pygame.Surface((100 * self.owner.keys, 30), pygame.SRCALPHA, 32)
+        self.image = pygame.Surface((KEY_SPRITE.get_width() * self.owner.keys, KEY_SPRITE.get_height()),
+                                    pygame.SRCALPHA, 32)
         # normal hearts
         for i in range(self.owner.keys):
-            self.image.blit(KEY_SPRITE, (i * 100, 0))
+            self.image.blit(KEY_SPRITE, (i * KEY_SPRITE.get_width(), 0))
+
+# TODO add stamina!!! heheheheh
