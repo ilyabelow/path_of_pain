02d7c60ca055a513a060555cb7ab189f96a4ad85
ilyabelow <belof.ilya@gmail.com>
2019-04-25 12:41:21 +0300

    moved level init to separate function

--- src/states/game.py
+++ src/states/game.py
@@ -19,16 +19,6 @@ class Game(State):
         self.WIN_SOUND = pygame.mixer.Sound('assets/sounds/secret_discovered_temp.wav')
         self.WIN_SOUND.set_volume(2)
 
-        # MUSIC INITIALIZATION
-        # TODO proper music controller
-        # TODO custom music!
-        if self.painful:
-            pygame.mixer.music.load('assets/sounds/Furious_Gods.wav')
-        else:
-            pygame.mixer.music.load('assets/sounds/Gods_and_Glory.wav')
-        pygame.mixer.music.set_volume(const.MUSIC_NORMAL_VOLUME)
-        pygame.mixer.music.play(loops=-1)
-
         # GROUPS INITIALIZATION
         # TODO move level generation to separate entity
         self.render_group = base.AdvancedLayeredUpdates()
@@ -52,53 +42,73 @@ class Game(State):
         self.heart_factory = pickupable.HeartFactory(self.pickupable_group)
         self.player_factory = player.PlayerFactory(self, self.player_group)
 
+        # LEVEL INITIALIZATION
+        self.level_rect = None
+        self.max_keys = 0
+        self.player = None
 
-        # LEVEL INITIALIZATIOB
-        self.level_rect = pygame.Rect(0, 0, 3000, 2000)
-        boxes_coords = (200, 200), (250, 200), (250, 250), (200, 350), (1300, 700), (1350, 750), (1450, 750), (
-            2400, 200), (2400, 400), (2450, 400), (2650, 350), (2400, 500), (2400, 700), (2500, 400), (
-                           2600, 300), (2600, 600), (2700, 400), (2500, 550), (2550, 500), (200, 650), (200, 700), (
-                           250, 700), (200, 750), (250, 750), (300, 750), (650, 150), (650, 200), (1100, 1700), (
-                           1150, 1700), (1200, 1700), (300, 1450), (250, 1500), (2400, 1450), (2450, 1450), (
-                           2500, 1450), (2550, 1450), (2600, 1450), (2400, 1700), (2450, 1700), (2500, 1700), (
-                           2550, 1700), (2600, 1700), (2350, 1500), (2350, 1450), (2350, 1550), (2350, 1600), (
-                           2350, 1650), (2350, 1700), (2600, 1500), (2600, 1550), (2600, 1600), (2600, 1650), (
-                           1800, 1750), (2050, 1700)
-        for box in boxes_coords:
-            self.box_factory.create(box)
-        enemies_coords = (500, 1300), (900, 1300), (500, 1300), (900, 1300), (500, 1600), (900, 1600), (700, 1450), \
-                         (1700, 450), (2200, 450), (2850, 150), (2850, 750), (2100, 1200), (2100, 1500), (1800, 1500), \
-                         (2450, 1600), (2500, 1600), (2450, 1550), (2500, 1550)
-        for coord in enemies_coords:
-            self.enemy_factory.create(coord)
-        self.max_keys = 5
-        self.distribute_keys()
-        # TODO redo walls input because now it is DISGUSTING
-        # vertical center walls
-        walls_rects = (1400, 900, 200, 500), (1400, 1600, 200, 300), (
-            # horizontal center walls
-            100, 900, 500, 200), (900, 900, 500, 200), (1600, 900, 500, 200), (2400, 900, 500, 200), (
-                          # pillars
-                          900, 400, 200, 200, 90), (1400, 400, 200, 200, 120), (1900, 400, 200, 200, 150), (
-                          1800, 1300, 200, 200, 30), (
-                          # border walls
-                          100, 0, 2800, 100), (0, 0, 100, 2000), (0, 1900, 3000, 150), (
-                          2900, 0, 100, 2000)
-        for wall in walls_rects:
-            self.wall_factory.create(wall[:4], wall[-1] if len(wall) == 5 else 50)  # TODO remove this bodge
+        self.room_num = 1
+        self.reset_level()
 
-        # PLAYER INITIALIZING
-        if pygame.joystick.get_count() == 0:
-            ctrlr = controller.Keyboard()
-        else:
-            ctrlr = controller.Joystick()
-        self.player = self.player_factory.create((400, 300), ctrlr)
+    def init_room(self):
+        # TODO factory deletion that are not in use
+        for v in dir(self):
+            if v.rfind('_group') != -1:
+                getattr(self, v).empty()
+        # TODO better switch-case
+        if self.room_num == 1:
+            self.level_rect = pygame.Rect(0, 0, 3000, 2000)
 
-        self.player.fetch_screen()
-        self.prev_rect = [self.window]
-        # TODO particle factory
-        self.fade = particle.Fade(const.GAME_FADE_IN, False, self.deploy_logo)
-        self.render_group.add(self.fade)
+            # MUSIC INITIALIZATION
+            # TODO proper music controller
+            if self.painful:
+                pygame.mixer.music.load('assets/sounds/Furious_Gods.wav')
+            else:
+                pygame.mixer.music.load('assets/sounds/Gods_and_Glory.wav')
+            pygame.mixer.music.set_volume(const.MUSIC_NORMAL_VOLUME)
+            pygame.mixer.music.play(loops=-1)
+
+            boxes_coords = (200, 200), (250, 200), (250, 250), (200, 350), (1300, 700), (1350, 750), (1450, 750), (
+                2400, 200), (2400, 400), (2450, 400), (2650, 350), (2400, 500), (2400, 700), (2500, 400), (
+                               2600, 300), (2600, 600), (2700, 400), (2500, 550), (2550, 500), (200, 650), (200, 700), (
+                               250, 700), (200, 750), (250, 750), (300, 750), (650, 150), (650, 200), (1100, 1700), (
+                               1150, 1700), (1200, 1700), (300, 1450), (250, 1500), (2400, 1450), (2450, 1450), (
+                               2500, 1450), (2550, 1450), (2600, 1450), (2400, 1700), (2450, 1700), (2500, 1700), (
+                               2550, 1700), (2600, 1700), (2350, 1500), (2350, 1450), (2350, 1550), (2350, 1600), (
+                               2350, 1650), (2350, 1700), (2600, 1500), (2600, 1550), (2600, 1600), (2600, 1650), (
+                               1800, 1750), (2050, 1700)
+            for box in boxes_coords:
+                self.box_factory.create(box)
+            enemies_coords = (500, 1300), (900, 1300), (500, 1300), (900, 1300), (500, 1600), (900, 1600), (700, 1450), \
+                             (1700, 450), (2200, 450), (2850, 150), (2850, 750), (2100, 1200), (2100, 1500), (
+                                 1800, 1500), \
+                             (2450, 1600), (2500, 1600), (2450, 1550), (2500, 1550)
+            for coord in enemies_coords:
+                self.enemy_factory.create(coord)
+            self.max_keys = 5
+            self.distribute_keys()
+
+            # TODO redo walls input because now it is DISGUSTING
+            # vertical center walls
+            walls_rects = (1400, 900, 200, 500), (1400, 1600, 200, 300), (
+                # horizontal center walls
+                100, 900, 500, 200), (900, 900, 500, 200), (1600, 900, 500, 200), (2400, 900, 500, 200), (
+                              # pillars
+                              900, 400, 200, 200, 90), (1400, 400, 200, 200, 120), (1900, 400, 200, 200, 150), (
+                              1800, 1300, 200, 200, 30), (
+                              # border walls
+                              100, 0, 2800, 100), (0, 0, 100, 2000), (0, 1900, 3000, 150), (
+                              2900, 0, 100, 2000)
+            for wall in walls_rects:
+                self.wall_factory.create(wall[:4], wall[-1] if len(wall) == 5 else 50)  # TODO remove this bodge
+
+            # PLAYER INITIALIZING
+            if pygame.joystick.get_count() == 0:
+                ctrlr = controller.Keyboard()
+            else:
+                ctrlr = controller.Joystick()
+            self.player = self.player_factory.create((400, 300), ctrlr)
+            self.player.fetch_screen()
 
     def distribute_keys(self):
         if len(self.enemy_group) < self.max_keys:
@@ -125,8 +135,12 @@ class Game(State):
         self.app.switch_state(menu.Menu())
 
     def reset_level(self):
-        # TODO reset level
-        self.app.switch_state(Game(self.painful))
+        self.init_room()
+        self.prev_rect = [self.window]
+        # TODO particle factory
+        self.fade = particle.Fade(const.GAME_FADE_IN, False, self.deploy_logo)
+        self.render_group.add(self.fade)
+        self.update()  # TODO remove
 
     def fade_out(self, action_after_faded):
         self.fade = particle.Fade(const.GAME_FADE_OUT, True, action_after_faded)
