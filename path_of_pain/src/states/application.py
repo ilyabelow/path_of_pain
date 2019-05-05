2c104797584e6c37b4cb851a45f26cba5b0ca8bf
ilyabelow <belof.ilya@gmail.com>
2019-05-05 19:26:37 +0300

    application docstrings

--- path_of_pain/src/states/application.py
+++ path_of_pain/src/states/application.py
@@ -1,3 +1,4 @@
+"""Module with class that controls pygame directly"""
 import pygame
 
 from path_of_pain.src.framework import const
@@ -6,7 +7,11 @@ from path_of_pain.src.states.menu import Menu
 
 # Context for STATE PATTERN
 class Application:
+    """
+    Class for creating pygame window, updating and drawing in it
+    """
     def __init__(self):
+        # Pygame config
         pygame.mixer.pre_init(22050, -16, 8, 64)
         pygame.init()
         pygame.mouse.set_visible(False)
@@ -14,22 +19,37 @@ class Application:
         pygame.display.set_caption("Path of Pain")
         pygame.display.set_icon(pygame.image.load(const.IMG_PATH + "enemy.png"))
 
-        self.clock = pygame.time.Clock()
         self.state = None
         self.switched = False
         self.switch_state(Menu())
+        # Joystick init
         if pygame.joystick.get_count() != 0:
             pygame.joystick.Joystick(0).init()
+        self.clock = pygame.time.Clock()
         self.running = True
 
     def switch_state(self, state):
+        """
+        Switches current state to state passed in arguments
+
+        Also remembers if state was changed in this iteration of main loop
+
+        :param state: new state
+        :return: None
+        """
         self.state = state
         self.state.app = self
         self.switched = True
 
     def run(self):
+        """
+        Runs the application: updates it every frame
+
+        :return: None
+        """
         while self.running:
             self.state.update()
+            # If state was switched after updating it will not be drawn not updated
             if self.switched:
                 self.switched = False
                 continue
@@ -39,5 +59,10 @@ class Application:
         pygame.quit()
 
     def stop(self):
+        """
+        Stors the application by breaking main loop
+
+        :return: None
+        """
         self.running = False
         self.switched = True  # It will not be redrawn right after stopping
