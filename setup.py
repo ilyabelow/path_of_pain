9f39f396087c3e8dd2b6822a7bd6cd6e5ad11903
ilyabelow <belof.ilya@gmail.com>
2019-05-05 18:35:58 +0300

    requirements

--- setup.py
+++ setup.py
@@ -1,19 +1,21 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+requirements = []
+
+with open("requirements.txt", "r") as f:
+    for line in f.readlines():
+        requirements.append(line[:line.find('==')])
 
 setuptools.setup(
     name="path_of_pain_daily_build",
-    version="0.0.2.dev1",
+    version="0.0.2.dev0",
     author="ilyabelow",
     author_email="belof.ilya@gmail.com",
     description="Simple top view slasher for a uni course on Python & Programming technologies & CI",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
     url="https://github.com/ilyabelow/path_of_pain",
     license='MIT',
     packages=setuptools.find_packages(),
+    install_requires=requirements,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
