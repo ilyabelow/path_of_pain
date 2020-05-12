b2dfebe24aa94480a770f561a6bae0bea214ffad
ilyabelow <belof.ilya@gmail.com>
2020-05-12 18:05:59 +0300

    added cool and new installation and launch scripts which may or may not work

---
+++ install.sh
@@ -0,0 +1,6 @@
+# run me with `bash install.sh`
+
+python -m venv venv
+source venv/bin/activate
+pip install pygame
+deactivate
---
+++ play.sh
@@ -0,0 +1,3 @@
+source venv/bin/activate
+python launch.py
+deactivate
